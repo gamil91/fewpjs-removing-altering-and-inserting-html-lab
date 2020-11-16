196e9948d5817e2d16ad7f568d76974309f39ce6
gamil91 <alexis_gamil99@yahoo.com.com>
2020-11-15 18:04:09 -0800

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.querySelector('main#main').remove
+document.querySelector('main#main').remove()
 let newHeader = document.createElement('h1')
 newHeader.id = 'victory'
 newHeader.innerHTML = 'Alex is the champion'
